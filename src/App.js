3068fb6e54bfc64324e25ce6f2dd9c7553584631
Prasy12 <prasanna.venkatesh@psiog.com>
2022-03-11 14:33:40 +0530

    Testing for redeployment.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
 	console.log('ENV', `${process.env.REACT_APP_ENDPOINT_URL}`);
-	return <div>Agnostiq!</div>;
+	return <div>Welcome to my application!</div>;
 }
 
 export default App;
